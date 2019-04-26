5fe2f77bcc7a6cf1505f1bfd6dc1e5089fff4ad5
Hyunho Cho <whgusgh1357@gmail.com>
2019-04-26 20:53:55 +1200

    forkify search and recipe view update

--- Forkify/src/js/models/Search.js
+++ Forkify/src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { key } from '../config';
 
 export default class Search {
     constructor(query) {
@@ -6,7 +7,6 @@ export default class Search {
     }
     
     async getResults() {
-        const key = '9acd8851470098a97b21e2696a39db69';
         try {
             const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
