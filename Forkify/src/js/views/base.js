cc42cbda9d0636254eb916001f1f0a18fcaefc13
Hyunho Cho <whgusgh1357@gmail.com>
2019-05-25 22:21:21 +1200

    forkify all basic functions

--- Forkify/src/js/views/base.js
+++ Forkify/src/js/views/base.js
@@ -3,7 +3,9 @@ export const elements = {
     searchInput: document.querySelector('.search__field'),
     searchRes: document.querySelector('.results'),
     searchResList: document.querySelector('.results__list'),
-    searchResPages: document.querySelector('.results__pages')
+    searchResPages: document.querySelector('.results__pages'),
+    recipe: document.querySelector('.recipe'),
+    shopping: document.querySelector('.shopping__list')
 };
 
 export const elementStrings = {
